7feddc1bbaf2e1e63465e9270c08a437bc38ef07
Yuri Klind <vykmnsk@gmail.com>
2018-01-14 14:15:36 +1100

    Case-insensitive file ext, skip orientation if not present

--- resize.py
+++ resize.py
@@ -1,4 +1,5 @@
 import os
+import re
 import glob
 from PIL import Image, ExifTags
 
@@ -8,18 +9,16 @@ SIZE = 1200, 1200
 if not os.path.exists(OUTDIR):
     os.makedirs(OUTDIR)
 
-for filename in glob.glob('*.JPG'):
+# for filename in glob.glob('*.JPG'):
+for filename in [f for f in glob.glob('*') if re.match('^.*\.JPG$', f, flags=re.IGNORECASE)]:
+    print 'processing {}/{}'.format(OUTDIR, filename)
     img = Image.open(filename)
 
     if hasattr(img, '_getexif'):  # only present in JPEGs
-        for orientation in ExifTags.TAGS.keys():
-            if ExifTags.TAGS[orientation] == 'Orientation':
-                break
-        e = img._getexif()       # returns None if no EXIF data
-        if e is not None:
-            exif = dict(e.items())
-            orientation = exif[orientation]
-
+        exif = img._getexif()       # returns None if no EXIF data
+        key = next((k for k,v in ExifTags.TAGS.items() if v == 'Orientation'), None)
+        if key and (key in exif):
+            orientation = exif[key]
             if orientation == 3:   img = img.transpose(Image.ROTATE_180)
             elif orientation == 6: img = img.transpose(Image.ROTATE_270)
             elif orientation == 8: img = img.transpose(Image.ROTATE_90)
@@ -27,5 +26,4 @@ for filename in glob.glob('*.JPG'):
     img.thumbnail(SIZE, Image.ANTIALIAS)
     img.save(os.path.join(OUTDIR, filename))
     img.close()
-
 
